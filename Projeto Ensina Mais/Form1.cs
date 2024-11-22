bb69e384e2b8d43c5cb5b4737dab71a83f471c62
Miguel Zimmermann Martins Silva <73042110+Zstraffey@users.noreply.github.com>
2024-11-22 10:59:23 -0300

    Sistema Ensina Mais
    Atualizado com o sistema de Login

--- Projeto Ensina Mais/Form1.cs
+++ Projeto Ensina Mais/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Projeto_Ensina_Mais
 {
@@ -16,8 +17,59 @@ namespace Projeto_Ensina_Mais
         {
             InitializeComponent();
 
-            tela_inicial_secretaria tela_inicial_teste = new tela_inicial_secretaria();
-            tela_inicial_teste.Show();
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connString = "SERVER=localhost;DATABASE=ensina_mais;UID=root;PASSWORD =;";
+            MySqlConnection conn = new MySqlConnection(connString);
+            conn.Open();
+
+            using (conn)
+            {
+                try
+                {
+                    
+
+                    // Query SQL para verificar os campos
+                    string query = "SELECT COUNT(*) FROM usuario WHERE codFunc = @codFunc AND senha = @senha";
+
+                    string query2 = "SELECT permissao FROM usuario WHERE codFunc = @codFunc AND senha = @senha";
+
+                    MySqlCommand comando = new MySqlCommand(query2, conn);
+
+                    string permissao2 = Convert.ToString(comando.ExecuteNonQuery());
+
+                    using (MySqlCommand command = new MySqlCommand(query, conn))
+                    {
+                        // Adiciona parâmetros para evitar SQL Injection
+                        command.Parameters.AddWithValue("@codFunc", textBox1.Text);
+                        command.Parameters.AddWithValue("@senha", textBox2.Text);
+
+                        // Executa a consulta
+                        int result = Convert.ToInt32(command.ExecuteScalar());
+
+                        if (result != null)
+                        {
+                            string permissao = result.ToString();
+
+                            // Verifica o tipo de permissão e abre a janela apropriada
+                            tela_inicial telaInicial = new tela_inicial(permissao, permissao2);
+                            telaInicial.Show();
+                            this.Hide();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Usuário ou senha incorretos.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao conectar ao banco de dados: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
---
+++ Projeto Ensina Mais/tela_inicial.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_Ensina_Mais
+{
+    public partial class tela_inicial : Form
+    {
+        private string permissao;
+        public tela_inicial(string permissao, string permissao2)
+        {
+            InitializeComponent();
+            this.permissao = permissao;
+            {
+                // Exibe a permissão na tela inicial ou utiliza para ajustar o comportamento da aplicação
+                MessageBox.Show("Bem-vindo(a)");
+
+                // Ajuste o comportamento da aplicação conforme a permissão
+                if (permissao == "pro")
+                {
+                    
+                }
+                else if (permissao == "sec")
+                {
+                   
+                }
+                else if (permissao == "adm")
+                {
+                    
+
+                }
+            }
+
+            label1.Text = permissao2;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AlunoEditar alunoEdit = new AlunoEditar();
+            alunoEdit.Show();
+        }
+
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
